51c67cd6e1c393df32d640d1e25f0d33efc0e5b9
Clement-76 <clement.patigny@gmail.com>
2018-11-29 17:07:49 +0100

    Ajout du css du menu et de l'accueil

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Billet simple pour l'Alaska</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/style.css">
 </head>
 
 <body>
