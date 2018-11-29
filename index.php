49cacc70b0b9caaadeb8af03a7a6a805712ce9a0
Clement-76 <clement.patigny@gmail.com>
2018-11-29 16:42:15 +0100

    Ajout du menu et de l'accueil

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Billet simple pour l'Alaska</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="close_menu">
+            <div></div>
+            <div></div>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">lorem</a></li>
+                <li><a href="#">lorem</a></li>
+                <li><a href="#">lorem</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="overlay"></div>
+
+    <div class="hamburger_menu">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+
+    <section id="home">
+        <h1 class="title">BILLET SIMPLE POUR L'ALASKA</h1>
+        <div>
+            <div class="arrow bottom"></div>
+        </div>
+    </section>
+    
+</body>
+
+</html>
